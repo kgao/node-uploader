41fdc99b62e3e309730c3a17e92a893c52cfc7ae
kgao <kevin.gao@nymag.com>
2015-06-17 18:52:03 -0400

    Add initial

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # node-uploader
-A async nodejs file uploader.
+An async file uploader with Express + Nodejs.
+
+
+## Get Start
+
+	$ npm install
+	$ npm start
+
+-> localhost:7777
---
+++ app.js
@@ -0,0 +1,39 @@
+/*Define dependencies.*/
+
+var express=require("express");
+var multer  = require('multer');
+var app=express();
+var done=false;
+
+/*Configure the multer.*/
+
+app.use(multer({ dest: 'Root/Uploads/',
+    rename: function (fieldname, filename) {
+        return filename+"_"+Date.now();
+    },
+    onFileUploadStart: function (file) {
+        console.log(file.originalname + ' is starting ...')
+    },
+    onFileUploadComplete: function (file) {
+        console.log(file.fieldname + ' uploaded to  ' + file.path)
+        done=true;
+    }
+}));
+
+/*Handling routes.*/
+
+app.get('/',function(req,res){
+    res.send('Hello, Uploader!');
+});
+
+app.post('/log',function(req,res){
+    if(done==true){
+        console.log(req.files);
+        res.end("File uploaded.");
+    }
+});
+
+/*Run the server.*/
+app.listen(7777,function(){
+    console.log("Working on port 7777");
+});
