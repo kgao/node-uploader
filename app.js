c42af2e4fb9ede87dae9bf151bd34bf55c4a4f5d
kgao <kevin.gao@nymag.com>
2015-06-18 11:59:56 -0400

    cleanup

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /*Define dependencies.*/
 
-var express=require("express");
+var express=require('express');
 var multer  = require('multer');
 var app=express();
 var done=false;
@@ -9,14 +9,14 @@ var done=false;
 
 app.use(multer({ dest: 'Root/Uploads/',
     rename: function (fieldname, filename) {
-        return filename+"_"+Date.now();
+        return filename + "_" + Date.now();
     },
     onFileUploadStart: function (file) {
         console.log(file.originalname + ' is starting ...')
     },
     onFileUploadComplete: function (file) {
         console.log(file.fieldname + ' uploaded to  ' + file.path)
-        done=true;
+        done = true;
     }
 }));
 
@@ -26,8 +26,8 @@ app.get('/',function(req,res){
     res.send('Hello, Uploader!');
 });
 
-app.post('/log',function(req,res){
-    if(done==true){
+app.post('/upload',function(req,res){
+    if(done == true){
         console.log(req.files);
         res.end("File uploaded.");
     }
